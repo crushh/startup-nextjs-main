7ea35c26801e84776ba15f15e2d12e18e9d6403c
crushh <30379665+crushh@users.noreply.github.com>
2024-05-14 16:16:20 +0800

    main

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="立即体验"
+            paragraph="加入我们，体验未来人工智能的无限潜力。让 GPT-4o 帮助你解锁新知，开启智能生活新篇章。"
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
